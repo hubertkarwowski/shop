e8ee8367730c7a7e0b410d9b2dfc2da6bde15c83
Hubert Karwowski <hubert.karwowski01@gmail.com>
2021-10-18 20:03:54 +0200

    dodanie signup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import { auth } from "./firebase/firebase-utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase-utils";
 import { Switch, Route } from "react-router-dom";
 
 import Homepage from "./pages/homepage/Homepage";
@@ -21,9 +21,21 @@ export default class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
-      console.log(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapShot) => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+          console.log(this.state);
+        });
+      }
+      this.setState({ currentUser: userAuth });
     });
   }
 
--- src/pages/signinAndSignout/signinAndSignout.js
+++ src/pages/signinAndSignout/signinAndSignout.js
@@ -3,11 +3,13 @@ import React from "react";
 import "./signinAndSignout.scss";
 
 import Signin from "../../components/signin/Signin";
+import SignUp from "../../components/Signup/Signup";
 
 function signinAndSignout() {
   return (
     <div className="sign-in-and-sign-out">
       <Signin />
+      <SignUp />
     </div>
   );
 }
